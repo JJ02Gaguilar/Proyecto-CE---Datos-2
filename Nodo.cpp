94a0cff040b1078e43003ca609aef0127d93ee9c
Adri <laslegoaventuras@gmail.com>
2025-03-08 15:54:33 -0600

    Initial commit

---
+++ Nodo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lasle on 3/8/2025.
+//
+
+#include "Nodo.h"
---
+++ Nodo.h
@@ -0,0 +1,16 @@
+//
+// Created by lasle on 3/8/2025.
+//
+
+#ifndef NODO_H
+#define NODO_H
+
+
+
+class Nodo {
+
+};
+
+
+
+#endif //NODO_H
